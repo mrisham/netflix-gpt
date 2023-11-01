1bf3922c9cec69176e3f1aca403548c84e61b5cd
Mridul Shukla <75447397+mrisham@users.noreply.github.com>
2023-11-02 01:07:25 +0530

    added sign-uo functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -12,19 +16,34 @@ const Login = () => {
         />
       </div>
       <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-75">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Name..."
+            className="py-4 my-4 w-full bg-gray-700 rounded-lg"
+          />
+        )}
         <input
           type="text"
           placeholder="E-mail Address"
-          className="py-4 my-4 w-full bg-gray-700"
+          className="py-4 my-4 w-full bg-gray-700 rounded-lg"
         />
         <input
           type="text"
           placeholder="Password"
-          className="py-4 my-4 w-full bg-gray-700"
+          className="py-4 my-4 w-full bg-gray-700 rounded-lg"
         />
-        <button className="bg-red-700 my-6 p-4 w-full">Sign In</button>
-        <p className="py-4">New to Netflix? Sign up now.</p>
+        <button className="bg-red-700 my-6 p-4 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign up now."
+            : "Already registered ! Please Sign In"}
+        </p>
       </form>
     </div>
   );
